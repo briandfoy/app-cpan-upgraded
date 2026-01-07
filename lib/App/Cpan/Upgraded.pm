3fa1ed3c7957b1cd05bb513af1d6d6ba0a286bb9
brian d foy <briandfoy@pobox.com>
2026-01-07 10:04:13 -0500

    Updated copyright year

--- lib/App/Cpan/Upgraded.pm
+++ lib/App/Cpan/Upgraded.pm
@@ -1739,7 +1739,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2001-2025, brian d foy, All Rights Reserved.
+Copyright (c) 2001-2026, brian d foy, All Rights Reserved.
 
 You may redistribute this under the same terms as Perl itself.
 
